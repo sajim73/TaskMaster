f7e7fb6a5bff04a60792f07f3ce5b8895bcea025
Sajim Ahmed <sajimahmed73@gmail.com>
2025-09-15 16:53:41 -0500

    Update validators.py

--- TaskMaster/backend/app/utils/validators.py
+++ TaskMaster/backend/app/utils/validators.py
@@ -1,1 +1,15 @@
 # Validators
+from datetime import datetime
+
+def validate_date(date_str):
+    """
+    Validate date string format YYYY-MM-DD or YYYY-MM-DD HH:MM:SS
+    """
+    try:
+        if len(date_str) == 10:
+            datetime.strptime(date_str, "%Y-%m-%d")
+        else:
+            datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        return True
+    except ValueError:
+        return False
